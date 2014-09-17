e9ed688f2c191847950c64e552f78f6b521c204d
fahad ali <fahadali@folio3.com>
2014-09-17 20:33:51 +0500

    smoke_upgraded_chrome_issue_resolved
    smoke_upgraded_chrome_issue_resolved

---
+++ HL_Smoke_chrome_isuue_resolved/HL_Smoke/HL_Smoke/HL_Base_Class.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Linq;
+using System.Drawing;                         // for taking screenshot
+using System.Drawing.Imaging;                 // for taking screenshot
+using System.Collections.Generic;
+using OpenQA.Selenium.Interactions;           // for Actions API
+using OpenQA.Selenium.Interactions.Internal;  // for Actions API
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Safari;
+using OpenQA.Selenium.Support.PageObjects;
+using System.Windows;
+using System.Linq;
+using System.Collections;
+
+namespace HL_Smoke
+{
+    public class HL_Base_Class
+    {
+
+        public string browser_name;
+
+        public void WaitForChrome(int counter, string browser_name)
+        {
+            if (browser_name.ToString() == "chrome")
+            {
+                Thread.Sleep(counter);
+            }
+
+        }
+    }
+
+}
